26a14ae234d1df5291f795aef65e37f83cad5ed2
Faisal ahmed <faisal2410@yahoo.com>
2023-01-12 17:04:43 +0600

    updated

--- src/components/cards/Jumbotron.js
+++ src/components/cards/Jumbotron.js
@@ -5,10 +5,13 @@ const Jumbotron = ({
   subTitle = "Welcome to React E-commerce",
 }) => {
   return (
-    <div className="container-fluid bg-primary">
+    <div
+      className="container-fluid jumbotron"
+      style={{ marginTop: "-8px", height: "200px" }}
+    >
       <div className="row">
-        <div className="col text-center p-5 bg-light">
-          <h1>{title}</h1>
+        <div className="col text-center p-5">
+          <h1 className="fw-bold">{title}</h1>
           <p className="lead">{subTitle}</p>
         </div>
       </div>
