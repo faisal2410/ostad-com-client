a446b3a849a13ad81e3584add214f07b7ea89f54
Faisal ahmed <faisal2410@yahoo.com>
2023-02-05 20:52:52 +0600

    test.js added

---
+++ src/pages/test.js
@@ -0,0 +1,7 @@
+// let number = 10;
+
+// let output = (number >= 10) ? ((number>=100)?("grater than 100"):("grater than 10 but less than 100")) : ((number>=0)?("grater than zero but less than 10"):("Negative number"))
+// console.log(output)
+
+
+// ()?(()?():()):(()?():())
