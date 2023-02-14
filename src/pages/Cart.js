1699abbd9539f5b22634c7c8ee308101ba185a71
Faisal ahmed <faisal2410@yahoo.com>
2023-02-14 18:51:57 +0600

    payment system added

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,7 +2,8 @@ import { useAuth } from "../context/auth";
 import { useCart } from "../context/cart";
 import Jumbotron from "../components/cards/Jumbotron";
 import { useNavigate } from "react-router-dom";
-import moment from "moment";
+import UserCartSidebar from "../components/cards/UserCartSidebar";
+import ProductCardHorizontal from "../components/cards/ProductCardHorizontal";
 
 const Cart=()=> {
   // context
@@ -10,25 +11,6 @@ const Cart=()=> {
   const [auth, setAuth] = useAuth();
   // hooks
   const navigate = useNavigate();
-
-  const removeFromCart = (productId) => {
-    let myCart = [...cart];
-    let index = myCart.findIndex((item) => item._id === productId);
-    myCart.splice(index, 1);
-    setCart(myCart);
-    localStorage.setItem("cart", JSON.stringify(myCart));
-  };
-
-  const cartTotal = () => {
-    let total = 0;
-    cart.map((item) => {
-      total += item.price;
-    });
-    return total.toLocaleString("en-US", {
-      style: "currency",
-      currency: "USD",
-    });
-  };
 
   return (
     <>
@@ -69,103 +51,12 @@ const Cart=()=> {
             <div className="col-md-8">
               <div className="row">
                 {cart?.map((p, index) => (
-                  <div
-                    key={index}
-                    className="card mb-3"
-                  // style={{ maxWidth: 540 }}
-                  >
-                    <div className="row g-0">
-                      <div className="col-md-4">
-                        <img
-                          src={`${process.env.REACT_APP_API}/product/photo/${p._id}`}
-                          alt={p.name}
-                          style={{
-                            height: "150px",
-                            width: "150px",
-                            objectFit: "cover",
-                            marginLeft: "-12px",
-                            borderRopRightRadius: "0px",
-                          }}
-                        />
-                      </div>
-                      <div className="col-md-8">
-                        <div className="card-body">
-                          <h5 className="card-title">
-                            {p.name}{" "}
-                            {p?.price?.toLocaleString("en-US", {
-                              style: "currency",
-                              currency: "USD",
-                            })}
-                          </h5>
-                          <p className="card-text">{`${p?.description?.substring(
-                            0,
-                            50
-                          )}..`}</p>
-                        </div>
-                      </div>
-
-                      <div className="d-flex justify-content-between">
-                        <p className="card-text">
-                          <small className="text-muted">
-                            Listed {moment(p.createdAt).fromNow()}
-                          </small>
-                        </p>
-                        <p
-                          className="text-danger mb-2 pointer"
-                          onClick={() => removeFromCart(p._id)}
-                        >
-                          Remove
-                        </p>
-                      </div>
-                    </div>
-                  </div>
+                  <ProductCardHorizontal key={index} p={p} />
                 ))}
               </div>
             </div>
 
-            <div className="col-md-4">
-              <h4>Your cart summary</h4>
-              Total / Address / Payments
-              <hr />
-              <h6>Total: {cartTotal()}</h6>
-              {auth?.user?.address ? (
-                <>
-                  <div className="mb-3">
-                    <hr />
-                    <h4>Address:</h4>
-                    <h5>{auth?.user?.address}</h5>
-                  </div>
-                  <button
-                    className="btn btn-outline-warning"
-                    onClick={() => navigate("/dashboard/user/profile")}
-                  >
-                    Update address
-                  </button>
-                </>
-              ) : (
-                <div className="mb-3">
-                  {auth?.token ? (
-                    <button
-                      className="btn btn-outline-warning"
-                      onClick={() => navigate("/dashboard/user/profile")}
-                    >
-                      Add delivery address
-                    </button>
-                  ) : (
-                    <button
-                      className="btn btn-outline-danger mt-3"
-                      onClick={() =>
-                        navigate("/login", {
-                          state: "/cart",
-                        })
-                      }
-                    >
-                      Login to checkout
-                    </button>
-                  )}
-                </div>
-              )}
-            </div>
+            <UserCartSidebar />
           </div>
         </div>
       )}
